614954e7c3a208b60c937e9daeaeda32a6605b73
mohammed3200 <wwyuu799@gmail.com>
2024-09-26 22:37:40 +0200

    change development mode in cleak

--- app/layout.tsx
+++ app/layout.tsx
@@ -35,6 +35,9 @@ export default function RootLayout({
           colorPrimary: "#3371FF",
           fontSize: "16px",
         },
+        layout: {
+          unsafe_disableDevelopmentModeWarnings: true,
+        },
       }}
     >
       <html lang="en">
