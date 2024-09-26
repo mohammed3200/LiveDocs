3476b9fbdc28e3ec95ddc0dfe402cc3c2100a799
mohammed3200 <wwyuu799@gmail.com>
2024-09-26 20:07:27 +0200

    this first commit

---
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -0,0 +1,14 @@
+import { SignUp } from '@clerk/nextjs'
+import React from 'react'
+
+const SignUpPage = () => {
+  return (
+    <main
+    className='auth-page'
+    >
+        <SignUp/>
+    </main>
+  )
+}
+
+export default SignUpPage
