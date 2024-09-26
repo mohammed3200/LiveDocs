f6b75722546951b026fc5e58455241b4bfe62692
mohammed3200 <wwyuu799@gmail.com>
2024-09-26 20:26:33 +0200

    ignoreBuildErrors:true

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,24 +1,21 @@
-import AddDocumentBtn from "@/components/AddDocumentBtn";
-import Header from "@/components/Header";
-import { Button } from "@/components/ui/button";
-import { SignedIn, UserButton } from "@clerk/nextjs";
-import { currentUser } from "@clerk/nextjs/server";
-import { redirect } from "next/navigation";
-import Image from "next/image";
-import { getDocuments } from "@/lib/actions/room.actions";
-import Link from "next/link";
-import { dateConverter } from "@/lib/utils";
-import { DeleteModal } from "@/components/DeleteModal";
-import Notifications from "@/components/Notifications";
-
+import AddDocumentBtn from '@/components/AddDocumentBtn';
+import { DeleteModal } from '@/components/DeleteModal';
+import Header from '@/components/Header'
+import Notifications from '@/components/Notifications';
+import { Button } from '@/components/ui/button'
+import { getDocuments } from '@/lib/actions/room.actions';
+import { dateConverter } from '@/lib/utils';
+import { SignedIn, UserButton } from '@clerk/nextjs'
+import { currentUser } from '@clerk/nextjs/server';
+import Image from 'next/image';
+import Link from 'next/link';
+import { redirect } from 'next/navigation';
 
 const Home = async () => {
   const clerkUser = await currentUser();
-  if (!clerkUser) redirect("/sign-in");
+  if(!clerkUser) redirect('/sign-in');
 
-  const roomDocuments = await getDocuments(
-    clerkUser.emailAddresses[0].emailAddress
-  );
+  const roomDocuments = await getDocuments(clerkUser.emailAddresses[0].emailAddress);
 
   return (
     <main className="home-container">
@@ -30,11 +27,12 @@ const Home = async () => {
           </SignedIn>
         </div>
       </Header>
+
       {roomDocuments.data.length > 0 ? (
         <div className="document-list-container">
           <div className="document-list-title">
             <h3 className="text-28-semibold">All documents</h3>
-            <AddDocumentBtn
+            <AddDocumentBtn 
               userId={clerkUser.id}
               email={clerkUser.emailAddresses[0].emailAddress}
             />
@@ -42,12 +40,9 @@ const Home = async () => {
           <ul className="document-ul">
             {roomDocuments.data.map(({ id, metadata, createdAt }: any) => (
               <li key={id} className="document-list-item">
-                <Link
-                  href={`/documents/${id}`}
-                  className="flex flex-1 items-center gap-4"
-                >
+                <Link href={`/documents/${id}`} className="flex flex-1 items-center gap-4">
                   <div className="hidden rounded-md bg-dark-500 p-2 sm:block">
-                    <Image
+                    <Image 
                       src="/assets/icons/doc.svg"
                       alt="file"
                       width={40}
@@ -56,33 +51,32 @@ const Home = async () => {
                   </div>
                   <div className="space-y-1">
                     <p className="line-clamp-1 text-lg">{metadata.title}</p>
-                    <p className="text-sm font-light text-blue-100">
-                      Created about {dateConverter(createdAt)}
-                    </p>
+                    <p className="text-sm font-light text-blue-100">Created about {dateConverter(createdAt)}</p>
                   </div>
                 </Link>
-                <DeleteModal roomId={id}/>
+                <DeleteModal roomId={id} />
               </li>
             ))}
           </ul>
         </div>
-      ) : (
+      ): (
         <div className="document-list-empty">
-          <Image
+          <Image 
             src="/assets/icons/doc.svg"
             alt="Document"
             width={40}
             height={40}
             className="mx-auto"
           />
-          <AddDocumentBtn
+
+          <AddDocumentBtn 
             userId={clerkUser.id}
             email={clerkUser.emailAddresses[0].emailAddress}
           />
         </div>
       )}
     </main>
-  );
-};
+  )
+}
 
-export default Home;
+export default Home
